61a837c3d45c39d384cb190196b74848c4d38537
zeneight <thezeneight@gmail.com>
2022-11-05 19:44:34 +0800

    uploading first code

---
+++ index.php
@@ -0,0 +1,20 @@
+<?php
+$var = "this is text";
+
+echo $var;
+echo "<br>";
+echo "<br>";
+
+
+// -----------------
+
+$color = "red";
+
+echo "My car is " . $color . "<br>";
+
+echo "<br>";
+
+$x = 5;
+$y = 4;
+echo $x + $y;
+?>
